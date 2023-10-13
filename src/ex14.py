c50415105410591902df7cc24a9bb2f8b5e6531e
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-13 23:35:21 +0700

    fix test average

--- src/ex14.py
+++ src/ex14.py
@@ -14,4 +14,11 @@ def average(num_list: list) -> float:
     - float: The average of the numbers in the list.
       If the list is empty, returns 0.
     """
-    pass
+    if len(num_list) == 0:
+        return None
+
+    total = 0
+
+    for i in num_list:
+        total += i
+    return total / len(num_list)
