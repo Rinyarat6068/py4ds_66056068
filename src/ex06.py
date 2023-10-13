a0d9cbe37dce21c3c870b08b36440ece4aef2694
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-13 19:11:24 +0700

    fix suffix

--- src/ex06.py
+++ src/ex06.py
@@ -13,7 +13,7 @@ def ordinal_suffix(num):
     Returns:
         str: The ordinal suffix corresponding to the given number.
     """
-    # TODO : complete this
+    # FIX : complete this
     if num == 0 or num >= 4 and num <= 100:
         return str(num)+"th"
     elif num == 1 or num == 101:
