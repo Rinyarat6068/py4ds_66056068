cf470901cc3e44a6d14e2d30d0b2636650f30d61
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-13 23:41:54 +0700

    fix test median

--- src/ex15.py
+++ src/ex15.py
@@ -3,7 +3,7 @@ Exercise 15
 """
 
 
-def median(params):
+def median(num_list):
     """
     Calculate the median of a list of numbers.
 
@@ -13,4 +13,14 @@ def median(params):
     Returns:
         [int, None]: The median value of the list, or None if the list is empty.
     """
-    pass
+    if len(num_list) == 0:
+        return None
+
+    num_list.sort()
+
+    middle_index = len(num_list) // 2
+
+    if len(num_list)%2 == 0:
+        return (num_list[middle_index] + num_list[middle_index - 1]) / 2
+    else:
+        return num_list[middle_index]
