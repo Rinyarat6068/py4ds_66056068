344ecea52f26e6bfe70fa9d3738e929f9f4900a6
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-14 00:34:48 +0700

    fix test gen password

--- src/ex19.py
+++ src/ex19.py
@@ -11,6 +11,17 @@ SPECIAL = '~!@#$%^&*()_+'                       # 13
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
+def gen_password(length):
     # TODO : complete this
-    pass
+    my_length = length
+    if length < 12:
+        my_length = 12
+
+    password = [LOWER_LETTERS[random.randint(0, 25)], UPPER_LETTERS[random.randint(0, 25)],
+                NUMBERS[random.randint(0, 9)], SPECIAL[random.randint(0, 12)]]
+
+    while len(password) < my_length:
+        password.append(ALL_CHARS[random.randint(0,74)])
+
+    random.shuffle(password)
+    return ''.join(password)
