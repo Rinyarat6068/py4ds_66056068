43a931e68d070289a919490f6160d0a507348b5c
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-14 01:33:12 +0700

    fix test mode

--- src/ex16.py
+++ src/ex16.py
@@ -3,7 +3,7 @@ Exercise 16
 """
 
 
-def mode(params):
+def mode(num_list):
     """
     Calculate the mode of a list of numbers.
 
@@ -13,4 +13,20 @@ def mode(params):
     Returns:
     - int or None: The mode of the list, or None if the list is empty.
     """
-    pass
+    if len(num_list) == 0:
+        return None
+
+    count = {}
+
+    most_freq_num = None
+    most_freq_num_count = 0
+
+    for i in num_list:
+        if i not in count:
+            count[i] = 0
+        count[i] += 1
+        if count[i] > most_freq_num_count:
+            most_freq_num = i
+            most_freq_num_count = count[i]
+    return most_freq_num
+
