90063432c3cd80cde94c7c2f1b8562eabcd03c7d
py4ds_66056068 <66056068@kmitl.ac.th>
2023-10-14 00:16:54 +0700

    fix get cost of coffee

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,8 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(num_cof, price_cof):
+    num_free_coffee = num_cof//9
+    num_paid_coffee = num_cof - num_free_coffee
+    return num_paid_coffee*price_cof
+
